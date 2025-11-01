6dac23075de0cdddc83b887bf9040d419efef3a3
Brandon Martin Del Campo Huerta <Brandonmartin@csu.fullerton.edu>
2025-10-31 18:40:21 -0700

    Initial commit

---
+++ src/test/java/com/SecuringWeb/Securing/a/Web/SecuringAWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SecuringWeb.Securing.a.Web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuringAWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
